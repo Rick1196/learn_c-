49d964b7aac938fca3486c415ee08dd067b0488b
Rick1196 <ricardompp11@gmail.com>
2022-07-13 20:38:20 -0500

    preprocessors

--- leaarning_c++/getting_started/basics.cpp
+++ leaarning_c++/getting_started/basics.cpp
@@ -1,5 +1,8 @@
 // a preprocessor directive that includes the content of the standard c++ header file
 #include <iostream>
+// preprocessor edit the source code, cutting some bits out changing
+// others and adding other thinks
+#define ZERO 0
 int multiply(int a, int b = 7);
 
 int multiply(int a, int b)
@@ -11,5 +14,5 @@ int main()
 {
    // std is a namespace, and :: is the scrope resolution operator
    std::cout << "Hello world!" << std::endl;
-   std::cout << multiply(7) << std::endl;
+   std::cout << multiply(ZERO) << std::endl;
 }
